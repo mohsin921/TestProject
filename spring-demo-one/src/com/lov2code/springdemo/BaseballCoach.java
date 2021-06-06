46c7f03449bc4e836ed97d9f1efbeb60b9e576d6
Mohsin Ali <Mohsin Ali@192.168.1.35>
2021-06-06 15:01:39 +0100

    First commit

---
+++ spring-demo-one/src/com/lov2code/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.lov2code.springdemo;
+
+public class BaseballCoach implements Coach {
+	public String getDailyWorkout() {
+		return "Baseball: Spend 30 mintues on batting practice";
+	}
+
+}
---
+++ spring-demo-one/src/com/lov2code/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.lov2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+
+		return "Run a hard 5k";
+	}
+
+}
